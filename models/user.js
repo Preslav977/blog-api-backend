e3584e2c3659fe6283c17901b79f59080bf23891
Preslav977 <p.cvetanow@gmail.com>
2024-04-15 09:34:19 +0300

    The user model has been designed.

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: { type: String, minLength: 5, maxLength: 30, required: true },
+  username: { type: String, minLength: 5, maxLength: 30, required: true },
+  first_name: { type: String, minLength: 5, maxLength: 30, required: true },
+  last_name: { type: String, minLength: 5, maxLength: 30, required: true },
+  password: { type: String, minLength: 8, maxLength: 30, required: true },
+  confirm_password: {
+    type: String,
+    minLength: 8,
+    maxLength: 30,
+    required: true,
+  },
+});
