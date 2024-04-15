d9c28755b5a6af7c0777d0d16b290b669379be1f
Preslav977 <p.cvetanow@gmail.com>
2024-04-15 10:02:21 +0300

    The comment model has been designed.

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const CommentSchema = new Schema({
+  content: { type: String, minLength: 5, maxLength: 100, required: true },
+  date: { type: Date },
+  like: { type: Number },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+module.exports = mongoose("Comment", CommentSchema);
