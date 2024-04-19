40d76b60e26bb26effe43aa20ed7034853104f90
Preslav977 <p.cvetanow@gmail.com>
2024-04-19 10:06:05 +0300

    Fetching and updating a post by id is implemented.

--- routes/post.js
+++ routes/post.js
@@ -102,9 +102,9 @@ router.get(
   }),
 );
 
-// router.get("/category/:name", (req, res) => {
-//   res.send(`GET HTTP post category name request ${req.params.name}`);
-// });
+router.get("/category/:name", (req, res) => {
+  // TODO: When the categories are created.
+});
 
 router.get(
   "/tag/:name",
@@ -125,8 +125,76 @@ router.get(
   }),
 );
 
-// router.get("/latest:/id", (req, res) => {
-//   res.send(`GET HTTP post latest post page number request ${req.params.id}`);
-// });
+router.get("/latest:/id", (req, res) => {
+  // TODO: When you figure out how to make 10 posts on each page.
+});
+
+router.put(
+  "/post/:id",
+
+  body("title", "Title must be between 5 and 80 characters long.")
+    .trim()
+    .isLength({ min: 5 })
+    .isLength({ max: 80 })
+    .escape(),
+  body("author", "Author must be between 5 and 80 characters long.")
+    .trim()
+    .isLength({ min: 5 })
+    .isLength({ max: 80 })
+    .escape(),
+  body("body", "Body must be between 5 and 300 characters long.")
+    .trim()
+    .isLength({ min: 5 })
+    .isLength({ max: 300 })
+    .escape(),
+  //   body("category", "Category must be between 5 and 30 characters long.")
+  //     .trim()
+  //     .isLength({ min: 5 })
+  //     .isLength({ max: 30 })
+  //     .escape(),
+  body("tags", "Tags must be 5 and 80 characters and 30 characters long.")
+    .trim()
+    .isLength({ min: 5 })
+    .isLength({ max: 30 })
+    .escape(),
+  body("image_owner", "Image_Owner must be between 5 and 30 characters long.")
+    .trim()
+    .isLength({ min: 5 })
+    .isLength({ max: 30 })
+    .escape(),
+  body("image_source", "Image_Source must be between 5 and 30 characters long.")
+    .trim()
+    .isLength({ min: 5 })
+    .isLength({ max: 30 })
+    .escape(),
+
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    const post = new Post({
+      title: req.body.title,
+      author: req.body.author,
+      date: new Date(),
+      body: req.body.body,
+      category: req.body.category,
+      tags: req.body.tags,
+      image_link: req.body.image_link,
+      image_owner: req.body.image_owner,
+      image_source: req.body.image_source,
+      privacy: req.body.privacy,
+      comments: req.body.comments,
+      _id: req.params.id,
+    });
+
+    console.log(post);
+
+    if (!errors.isEmpty()) {
+      console.log(errors);
+    } else {
+      const updatePost = await Post.findByIdAndUpdate(req.params.id, post);
+      res.json(updatePost);
+    }
+  }),
+);
 
 module.exports = router;
